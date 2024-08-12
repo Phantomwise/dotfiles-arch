dbb0438615ed2fe3d6ab8eebe68e48ac4ab58ff3
Phantomwise <neverseenbywakingeyes@riseup.net>
2024-08-12 03:54:05 +0200

    🎨 Fix variables case

--- arch/home/.config/dunst/battery-alert.sh
+++ arch/home/.config/dunst/battery-alert.sh
@@ -3,34 +3,34 @@
 export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
-WARNING_LEVEL=25
-CRITICAL_LEVEL=10
+warning_level=25
+critical_level=10
 
 # Extract battery status
-BATTERY_DISCHARGING=$(acpi -b | grep "Battery 0" | grep -c "Discharging")
-BATTERY_LEVEL=$(acpi -b | grep "Battery 0" | grep -P -o '[0-9]+(?=%)')
+battery_discharging=$(acpi -b | grep "Battery 0" | grep -c "Discharging")
+battery_level=$(acpi -b | grep "Battery 0" | grep -P -o '[0-9]+(?=%)')
 
-FULL_FILE=/tmp/batteryfull
-EMPTY_FILE=/tmp/batteryempty
-CRITICAL_FILE=/tmp/batterycritical
+full_file=/tmp/batteryfull
+low_file=/tmp/batterylow
+critical_file=/tmp/batterycritical
 
 # Remove existing notifications if battery is charging
-if [ "$BATTERY_DISCHARGING" -eq 0 ] && [ -f $FULL_FILE ]; then
-    rm $FULL_FILE
-elif [ "$BATTERY_DISCHARGING" -eq 1 ] && [ -f $EMPTY_FILE ]; then
-    rm $EMPTY_FILE
+if [ "$battery_discharging" -eq 0 ] && [ -f $full_file ]; then
+    rm $full_file
+elif [ "$battery_discharging" -eq 1 ] && [ -f $low_file ]; then
+    rm $low_file
 fi
 
 # Check battery levels and send notifications
-if [ -n "$BATTERY_LEVEL" ]; then
-    if [ "$BATTERY_LEVEL" -le $CRITICAL_LEVEL ] && [ "$BATTERY_DISCHARGING" -eq 1 ]; then
+if [ -n "$battery_level" ]; then
+    if [ "$battery_level" -le $critical_level ] && [ "$battery_discharging" -eq 1 ]; then
         notify-send "Battery Critical" "The computer will shutdown soon." -u critical -i "battery-v-alert" -r 9991
-        touch $CRITICAL_FILE
-    elif [ "$BATTERY_LEVEL" -le $WARNING_LEVEL ] && [ "$BATTERY_DISCHARGING" -eq 1 ] && [ ! -f $EMPTY_FILE ]; then
-        notify-send "Low Battery" "${BATTERY_LEVEL}% of battery remaining." -u normal -i "battery-v-alert" -r 9991
-        touch $EMPTY_FILE
-    elif [ "$BATTERY_LEVEL" -gt 99 ] && [ "$BATTERY_DISCHARGING" -eq 0 ] && [ ! -f $FULL_FILE ]; then
+        touch $critical_file
+    elif [ "$battery_level" -le $warning_level ] && [ "$battery_discharging" -eq 1 ] && [ ! -f $low_file ]; then
+        notify-send "Low Battery" "${battery_level}% of battery remaining." -u normal -i "battery-v-alert" -r 9991
+        touch $low_file
+    elif [ "$battery_level" -gt 99 ] && [ "$battery_discharging" -eq 0 ] && [ ! -f $full_file ]; then
         notify-send "Battery Charged" "Battery is fully charged." -u normal -i "battery-v-100" -r 9991
-        touch $FULL_FILE
+        touch $full_file
     fi
 fi
