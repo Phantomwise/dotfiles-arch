e9347226ef326b5471d833303fbdf1eb540d52a4
Phantomwise <neverseenbywakingeyes@riseup.net>
2024-08-18 19:04:53 +0200

    ✨ 💩 Fake screensaver using systemd and pipes.c -- kinda works but not well

---
+++ arch/home/.local/bin/scripts/screensaver-pipes-fs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Fake screensaver using pipes.c
+# Replace "hyprctl dispatch fullscreen 0" with whatever command makes the active window fullscreen on your system.
+
+# Fullscreen the window and run cpipes
+hyprctl dispatch fullscreen 0
+cpipes -C ═║╗╝╚╔ -p 5
---
+++ arch/home/.local/bin/scripts/screensaver-pipes-wrapper.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Fake screensaver using pipes.c
+# Wrapper srcipt for pipes-fs.sh
+
+# Run pipes-fs.sh in a new terminal window
+kitty -1 -e screensaver-pipes-fs.sh
