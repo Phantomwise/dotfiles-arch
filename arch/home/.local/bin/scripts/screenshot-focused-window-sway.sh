8c6657629fa81559dfd54a15f73c01baa98627df
Phantomwise <neverseenbywakingeyes@riseup.net>
2024-09-14 03:06:04 +0200

    ✨ Add script to take a screenshot of the active window (Sway)

---
+++ arch/home/.local/bin/scripts/screenshot-focused-window-sway.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# Get the geometry of the focused window
+geometry=$(swaymsg -t get_tree | jq -r 'recurse(.nodes[]?, .floating_nodes[]?) | select(.focused).rect | "\(.x),\(.y) \(.width)x\(.height)"')
+
+# Get the class of the focused window
+window_class=$(swaymsg -t get_tree | jq -r 'recurse(.nodes[]?, .floating_nodes[]?) | select(.focused).app_id')
+
+# Take the screenshot with the window class in the filename
+grim -t png -g "$geometry" "$(xdg-user-dir PICTURES)/Screenshots/Screenshot_$(date +'%Y-%m-%d_%H-%M-%S')_${window_class}.png"
