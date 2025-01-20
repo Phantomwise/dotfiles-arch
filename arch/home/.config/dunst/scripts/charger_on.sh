0786e06f46896e5f0461232a0b3439343cc37ab8
Phantomwise <neverseenbywakingeyes@riseup.net>
2025-01-20 16:34:49 +0100

    🔥 Delete old notifications scripts

--- arch/home/.config/dunst/scripts/charger_on.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Send notification
-dunstify "Battery Charger Connected" "Laptop is now charging." -u normal -r 99114103 -i battery_plus
-# notify-send "Charger connected"
-
-# Log the event
-echo "$(date): Charger connected" >> /tmp/charger_log.txt
