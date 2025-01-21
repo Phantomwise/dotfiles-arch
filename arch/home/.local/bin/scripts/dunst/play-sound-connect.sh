cbae6e98158e2c2b51f2b7df3a89677a2658afb3
Phantomwise <neverseenbywakingeyes@riseup.net>
2025-01-21 10:49:20 +0100

    🍱 Add scripts to play sounds

--- arch/home/.local/bin/scripts/dunst/play-sound-connect.sh
+++ arch/home/.local/bin/scripts/dunst/play-sound-connect.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Path to the sound file
-SOUND_FILE="/usr/share/sounds/phantomwise/critical.wav"
+SOUND_FILE="/usr/share/sounds/phantomwise/connect.wav"
 
 # Play the sound with pw-play (pipewire)
 pw-play "$SOUND_FILE"
--- arch/home/.local/bin/scripts/dunst/play-sound-disconnect.sh
+++ arch/home/.local/bin/scripts/dunst/play-sound-disconnect.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Path to the sound file
-SOUND_FILE="/usr/share/sounds/phantomwise/critical.wav"
+SOUND_FILE="/usr/share/sounds/phantomwise/disconnect.mp3"
 
 # Play the sound with pw-play (pipewire)
 pw-play "$SOUND_FILE"
