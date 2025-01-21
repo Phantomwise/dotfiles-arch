2986587f991fcbf95013bc9a2cd337c41ef65e10
Phantomwise <neverseenbywakingeyes@riseup.net>
2025-01-21 11:25:07 +0100

    🐛 Fix the multiple notifications issue

--- arch/home/.local/bin/scripts/dunst/charger_notifications_udev.sh
+++ arch/home/.local/bin/scripts/dunst/charger_notifications_udev.sh
@@ -1,27 +1,52 @@
 #!/bin/bash
 
-# Check the argument
-if [ "$1" == "connected" ]; then
-    charger_status="connected"
-    icon="charger-plugged"
-    message_id="99114103"
-elif [ "$1" == "disconnected" ]; then
-    charger_status="disconnected"
-    icon="charger-unplugged"
-    message_id="99114103"
-else
-    echo "Invalid argument. Use 'connected' or 'disconnected'."
-    exit 1
-fi
+lockfile="/tmp/charger_notifications_udev.lock"
+statefile="/tmp/charger_state"
 
-# Log the event
-echo "$(date) $charger_status" >> /tmp/udev-61-charger-rules.log
+# Use flock to create a lock and ensure only one instance runs
+{
+    flock -n 9 || exit 1
 
-# Get the user who is currently logged in
-user=$(who | awk '{print $1}' | head -n 1)
+    # Check the argument
+    if [ "$1" == "connected" ]; then
+        charger_status="connected"
+        icon="charger-plugged"
+        message_id="99114103"
+    elif [ "$1" == "disconnected" ]; then
+        charger_status="disconnected"
+        icon="charger-unplugged"
+        message_id="99114103"
+    else
+        echo "Invalid argument. Use 'connected' or 'disconnected'."
+        exit 1
+    fi
 
-# Export the DBUS_SESSION_BUS_ADDRESS
-export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u $user)/bus"
+    # Read the last state
+    if [ -f "$statefile" ]; then
+        last_status=$(cat "$statefile")
+    else
+        last_status=""
+    fi
 
-# Run dunstify as the logged-in user with a specific message ID and icon
-su -c "DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS dunstify -r '$message_id' -i '$icon' 'Charger' 'Charger $charger_status'" $user
+    # Only proceed if the state has changed
+    if [ "$charger_status" != "$last_status" ]; then
+        # Log the event
+        echo "$(date) $charger_status" >> /tmp/udev-61-charger-rules.log
+
+        # Get the user who is currently logged in
+        user=$(who | awk '{print $1}' | head -n 1)
+
+        # Export the DBUS_SESSION_BUS_ADDRESS
+        export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u $user)/bus"
+
+        # Run dunstify as the logged-in user with a specific message ID and icon
+        su -c "DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS dunstify -r '$message_id' -i '$icon' 'Charger' 'Charger $charger_status'" $user
+
+        # Update the state file
+        echo "$charger_status" > "$statefile"
+    fi
+
+    # Add a small delay to prevent race conditions
+    sleep 2
+
+} 9>"$lockfile"
