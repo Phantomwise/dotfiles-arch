4aacdc219817169ed5bd5ab1bf71ecb84973482f
Phantomwise <neverseenbywakingeyes@riseup.net>
2025-01-21 10:53:39 +0100

    🚧 Modify notification

--- arch/home/.local/bin/scripts/dunst/charger_notifications_udev.sh
+++ arch/home/.local/bin/scripts/dunst/charger_notifications_udev.sh
@@ -2,11 +2,11 @@
 
 # Check the argument
 if [ "$1" == "connected" ]; then
-    charger_status="Charger connected"
+    charger_status="connected"
     icon="charger-plugged"
     message_id="99114103"
 elif [ "$1" == "disconnected" ]; then
-    charger_status="Charger disconnected"
+    charger_status="disconnected"
     icon="charger-unplugged"
     message_id="99114103"
 else
@@ -24,4 +24,4 @@ USER=$(who | awk '{print $1}' | head -n 1)
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u $USER)/bus"
 
 # Run dunstify as the logged-in user with a specific message ID and icon
-su -c "DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS dunstify -r '$message_id' -i '$icon' '$charger_status'" $USER
+su -c "DISPLAY=:0 DBUS_SESSION_BUS_ADDRESS=$DBUS_SESSION_BUS_ADDRESS dunstify -r '$message_id' -i '$icon' 'Charger' 'Charger $charger_status'" $USER
