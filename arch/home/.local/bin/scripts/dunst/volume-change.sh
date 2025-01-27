29820b28008260a3eae726fbd550cd9f8df71558
Phantomwise <neverseenbywakingeyes@riseup.net>
2025-01-27 12:40:09 +0100

    ♻️ Set notification icons in `dunstrc` with notification categories (instead of doing it directly with flags in each dunstify command)

--- arch/home/.local/bin/scripts/dunst/volume-change.sh
+++ arch/home/.local/bin/scripts/dunst/volume-change.sh
@@ -8,4 +8,4 @@ VOLUME_PERCENTAGE=$(echo "$VOLUME_OUTPUT" | grep -oP '\d+(\.\d+)?' | awk '{print
 echo "Volume Percentage: $VOLUME_PERCENTAGE"
 
 # Send the notification with a progress bar and custom highlight color
-dunstify "Volume" "Volume Level: $VOLUME_PERCENTAGE%" -u normal -r 118111108 -i volume-medium -h "int:value:$VOLUME_PERCENTAGE" -h "string:hlcolor:#FFFFFF"
+dunstify "Volume" "Volume Level: $VOLUME_PERCENTAGE%" -u normal -r 118111108 -h "int:value:$VOLUME_PERCENTAGE" -h "string:hlcolor:#FFFFFF" -h "string:category:volume.change"
--- arch/home/.local/bin/scripts/dunst/volume-mute.sh
+++ arch/home/.local/bin/scripts/dunst/volume-mute.sh
@@ -5,7 +5,7 @@ volume_status=$(wpctl get-volume @DEFAULT_AUDIO_SINK@)
 
 # Parse the output to check if muted
 if echo "$volume_status" | grep -q "[MUTED]"; then
-    dunstify "Volume Muted" -u normal -r 118111108 -i volume-mute
+    dunstify "Volume Muted" -u normal -r 118111108 -h "string:category:volume.mute"
 else
-    dunstify "Volume Unmuted" -u normal -r 118111108 -i volume-unmute
+    dunstify "Volume Unmuted" -u normal -r 118111108 -h "string:category:volume.unmute"
 fi
