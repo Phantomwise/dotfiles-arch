cdbf611a3414b3a74fdade613d884ce11e67384b
Phantomwise <neverseenbywakingeyes@riseup.net>
2025-04-01 18:09:00 +0200

    🔧 Add urgency, ID and category to the checkupdates notification

--- arch/home/.local/bin/scripts/dunst/check-updates.sh
+++ arch/home/.local/bin/scripts/dunst/check-updates.sh
@@ -5,5 +5,5 @@ updates=$(checkupdates 2> /dev/null | wc -l)
 
 # If there are updates, send a notification
 if [ "$updates" -gt 0 ]; then
-    dunstify "System Updates" "There are $updates updates available."
+    dunstify "System Updates" "There are $updates updates available." -u normal -r 117112100 -h "string:category:updates"
 fi
